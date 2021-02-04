c0b96f80ce2389fb62fb0ad81a86bd6b8def7317
moroni1995 <u388764730@byui.edu>
2021-02-04 11:09:40 -0300

    added some comments to external js file

--- js/poundsToKilograms.js
+++ js/poundsToKilograms.js
@@ -1,12 +1,12 @@
 function poundsToKilograms() {
-    //INPUT: Take the string in id="pounds and define kilograms constant"
+    //INPUT: Take the string in id="pounds" and define kilograms constant.
     const kilograms = 0.45359237;
     let userInput = document.getElementById("pounds").value;
-    //PROCESSING: Convert the user input to floating point number and mutiply it by kilograms
-    //After that, the result must be rounded to one decimal
+    //PROCESSING: Convert the user input to floating point number and mutiply it by kilograms,
+    //After that, the result must be rounded to one decimal.
     let pounds = parseFloat(userInput);
     let result = pounds * kilograms;
-    //OUTPUT: Display the conversion result in id="output" using the round method ".tofixed() and string interpolation" 
+    //OUTPUT: Displays the conversion result in id="output" using the round method ".tofixed()" and string interpolation. 
     document.getElementById("output").innerHTML = `${result.toFixed(1)} Kilograms`;    
 }
 
