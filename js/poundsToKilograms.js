b9192a7616edc3fd4e4134647e3ab648b04cadfc
moroni1995 <u388764730@byui.edu>
2021-02-04 11:24:14 -0300

    pseudo code modified

--- js/poundsToKilograms.js
+++ js/poundsToKilograms.js
@@ -2,8 +2,7 @@ function poundsToKilograms() {
     //INPUT: Take the string in id="pounds" and define kilograms constant.
     const kilograms = 0.45359237;
     let userInput = document.getElementById("pounds").value;
-    //PROCESSING: Convert the user input to floating point number and mutiply it by kilograms,
-    //After that, the result must be rounded to one decimal.
+    //PROCESSING: Convert the user input to a floating-point number and multiply it by kilograms.
     let pounds = parseFloat(userInput);
     let result = pounds * kilograms;
     //OUTPUT: Displays the conversion result in id="output" using the round method ".tofixed()" and string interpolation. 
