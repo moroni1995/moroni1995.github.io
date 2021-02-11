72d1ba2276f86c6da3a580329aff6de77dc005d6
moroni1995 <u388764730@byui.edu>
2021-02-11 13:05:42 -0300

    created calculator.html

---
+++ js/simpleCalc.js
@@ -0,0 +1,33 @@
+function checkAnswer() {
+    //INPUT = Getting user inputs and assigning it to variables
+    let number1 = parseFloat(document.getElementById('number1').value);
+    let number2 = parseFloat(document.getElementById('number2').value);
+    let operator = document.getElementById('operator').value;
+    let userAnswer = parseFloat(document.getElementById('answer').value);
+    let correctAnswer;
+
+    //PROCESSING = Using switch statement to verify which of the arithmetic operators 
+    //were selected and calculates the answer
+    switch (operator) {
+        case '+':
+            correctAnswer = number1 + number2;
+            break;
+        case '-':
+            correctAnswer = number1 - number2;
+            break;
+        case '/':
+            correctAnswer = number1 / number2;
+            break;
+        case '*':
+            correctAnswer = number1 * number2;
+            break;
+    }
+    //OUTPUT = Compares the user answer with the answer calculated by the computer
+    // and if the result is the same it will display "Correct", else will display "Incorrect"
+    if (userAnswer == correctAnswer) {
+        document.getElementById('output').innerHTML = "Correct! 🏆"
+    } else {
+        document.getElementById('output').innerHTML = "Incorrect! 😢"
+    }
+
+}
