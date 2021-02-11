9f604d230698c4a52cf2af25872355b3deda832a
moroni1995 <u388764730@byui.edu>
2021-02-11 14:24:34 -0300

    created footwear.html + js file

---
+++ js/weatherShoes.js
@@ -0,0 +1,23 @@
+function findShoes() {
+    //INPUT = Get user input of select element
+    let weather = document.getElementById('weather-select').value;
+    let footwear = 'No weather selected';
+
+    //PROCESS = Compares user input or weather variable and set footwear that matches with selected weather
+    switch (weather) {
+        case 'hot':
+            footwear = 'sandals';
+            break;
+        case 'rain':
+            footwear = 'galoshes';
+            break;
+        case 'snow':
+            footwear = 'boots';
+            break;
+        case 'unknown':
+            footwear = 'shoes';
+            break;
+    }
+    //OUTPUT = Display the footwear to the user
+    document.getElementById('output').innerHTML = `Footwear: ${footwear}`
+}
