615c74b3973376cc2a4e9f9bd18673f4430ce77a
moroni1995 <u388764730@byui.edu>
2021-02-11 13:09:58 -0300

    added some comments

--- js/simpleCalc.js
+++ js/simpleCalc.js
@@ -1,5 +1,5 @@
 function checkAnswer() {
-    //INPUT = Getting user inputs and assigning it to variables
+    //INPUT = Getting user inputs and assigning them to variables
     let number1 = parseFloat(document.getElementById('number1').value);
     let number2 = parseFloat(document.getElementById('number2').value);
     let operator = document.getElementById('operator').value;
